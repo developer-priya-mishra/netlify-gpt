f7500e66b48190938eb00e8f8595e4e31ad09b1d
Priya Mishra <119393765+developer-priya-mishra@users.noreply.github.com>
2025-04-16 14:56:46 +0530

    "installed Tailwind"

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 const App = () => {
   return (
     <>
-      <h1>Hii</h1>
+      <h1 className='text-3xl font-bold text-green-800'>Namste everyone! Let's build Netflix GPT</h1>
     </>
   )
 }
