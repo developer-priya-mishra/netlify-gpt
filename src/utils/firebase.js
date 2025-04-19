b0991da8af1b73553551b90f52b934a19912e3cc
Priya Mishra <119393765+developer-priya-mishra@users.noreply.github.com>
2025-04-19 21:40:09 +0530

    "Some changes"

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,21 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDNvc1PBh45hUzNGkfxBXLyXN0tF9v9oNw",
-  authDomain: "netlifygpt-663d0.firebaseapp.com",
-  projectId: "netlifygpt-663d0",
-  storageBucket: "netlifygpt-663d0.firebasestorage.app",
-  messagingSenderId: "772568556505",
-  appId: "1:772568556505:web:b500c2dd8a562dd2372fc8",
-  measurementId: "G-LJ5XQBKYME"
+  apiKey: "AIzaSyDmS-BX1hXj8qrMONO1O46u5FlDSYKw0js",
+  authDomain: "netlify-gpt.firebaseapp.com",
+  projectId: "netlify-gpt",
+  storageBucket: "netlify-gpt.firebasestorage.app",
+  messagingSenderId: "1032346184740",
+  appId: "1:1032346184740:web:aab70c661e08a80e50fcb1"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
