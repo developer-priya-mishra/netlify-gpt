d40f432439e7f60862cec5b9e1db85930867035a
Priya Mishra <119393765+developer-priya-mishra@users.noreply.github.com>
2025-04-21 11:06:16 +0530

    "Added auth and hosted the web"

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRFiKdNA2YnlgR1bB_1Bs2tD25H46YcrY",
+  authDomain: "netlify-gpt-f20c3.firebaseapp.com",
+  projectId: "netlify-gpt-f20c3",
+  storageBucket: "netlify-gpt-f20c3.firebasestorage.app",
+  messagingSenderId: "998075629777",
+  appId: "1:998075629777:web:13ce8bbb8dfab5d8c045f0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
