54fb455342f27c1c21c79e75de2c4572a076d676
Priya Mishra <119393765+developer-priya-mishra@users.noreply.github.com>
2025-04-24 15:26:08 +0530

    Some fixes

--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -2,18 +2,15 @@ import { useSelector } from "react-redux";
 import MovieList from "./MovieList";
 
 const SecondaryContainer = () => {
-  const nowPlayingMovies = useSelector(store => store.movies?.nowPlayingMovies)
-  const popularMovies = useSelector(store => store.movies?.popularMovies)  
-  const topRatedMovies = useSelector(store => store.movies?.topRatedMovies)
-  const upcomingMovies = useSelector(store => store.movies?.upcomingMovies)
+  const movies = useSelector(store => store.movies)
 
   return (
     <div className="bg-black">
       <div className="-my-40 relative z-20 pl-5">
-        <MovieList title={"Now Playing"} movies={nowPlayingMovies} />
-        <MovieList title={"Popular"} movies={popularMovies} />
-        <MovieList title={"Top Rated"} movies={topRatedMovies} />
-        <MovieList title={"Upcoming"} movies={upcomingMovies} />
+        <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
+        <MovieList title={"Popular"} movies={movies?.popularMovies} />
+        <MovieList title={"Top Rated"} movies={movies?.topRatedMovies} />
+        <MovieList title={"Upcoming"} movies={movies?.upcomingMovies} />
       </div>
     </div>
   );
